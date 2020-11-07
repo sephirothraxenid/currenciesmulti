16fc7ea2afc4c3d00b3d040cd311c6ffe0670251
SephirothRXND <74086928+sephirothraxenid@users.noreply.github.com>
2020-11-06 23:10:50 -0600

    Update esES.lua
    Is for a esES, no for esMX

--- Core/languages/esES.lua
+++ Core/languages/esES.lua
@@ -8,38 +8,148 @@
 
 local _, L = ...;
 if GetLocale() == "esES" then
+------ Bfa Pack
+--War Resources 
+L["WResLabel"] = "Recursos gerra: "
+L["WResTitle"] = "Recursos de guerra"
+L["WResDescription"] = "Se usan para reclutar tropas, realizar misiones\re investigar mejoras para el esfuerzo de guerra."
+--Titan Residuum 
+L["TitanTitanLabel"] = "Residuo titánico: "
+L["TitanTitanTitle"] = "Residuo titánico"
+L["TitanTitanDescription"] = "Restos desactivados de armadura de azerita valiosos\rpara los comerciantes etéreos de Boralus y Zandalar.\rObtenidos de cofres semanales de piedra angular mítica\ry al desguazar o desencantar armadura de azerita."
+--Service Medal
+L["SMedalLabel"] = "Medalla de servicio: "
+L["SMedalATitle"] = "Medalla de servicio de la Séptima Legión"
+L["SMedalHTitle"] = "Medalla de servicio de los Defensores del Honor"
+L["SMedalDescription"] = "Un distintivo de mérito por actos realizados en los campos de batalla\rde Kul Tiras, Zandalar y los frentes de guerra activos."
+--Seal of Wartorn Fate
+L["WartornLabel"] = "Sello destrozado: "
+L["WartornTitle"] = "Sello de destino destrozado"
+L["WartornDescription"] = "Altera el destino para ofrecer una oportunidad más\rde obtener botín de jefes de bandas y mazmorras de Battle for Azeroth."
+L["NoWartorn"] = "Puede adquirirse con Tezran en Boralus o Zurvan en Dazar'alor"
+--Seafarer's Dubloon
+L["DubloonLabel"] = "Doblón navegante: "
+L["DubloonTitle"] = "Doblón de navegante"
+L["DubloonDescription"] = "La divisa que utilizan las tribus del Mare Magnum.\rSe usa para obtener objetos de comerciantes en las expediciones insulares."
+--Prismatic Manapearl
+L["PearlLabel"] = "Perla maná: "
+L["PearlTitle"] = "Perla de maná centelleante"
+L["PearlDescription"] = "Una esfera brillante de energía Arcana encontrada en Nazjatar. \rSe puede comerciar con ella para obtener objetos poderosos y poco comunes \ro se puede usar para potenciar la armadura bentónica."
+--Coalescing Visions
+L["CoaviLabel"] = "Visiones aglomeradas: "
+L["CoaviTitle"] = "Visiones aglomeradas"
+L["CoaviDescription"] = "Wrathion puede manipular estas visiones \rpara introducirlas en un receptáculo que te permitirá \robtener acceso a una visión horripilante de N'Zoth."
+L["NoCoavi"] = "Disponible en el contenido de las Visiones de N'Zoth."
+--Corrupted Memento
+L["CMementoLabel"] = "Recuerdo corrupto: "
+L["CMementoTitle"] = "Recuerdo corrupto"
+L["CMementoDescription"] = "Fragmentos de recuerdos de las visiones horripilantes de N'Zoth,\rque se pueden analizar en el archivo de investigación titánico\r en la Cámara del Corazón."
+L["NoCMemento"] = "Disponible en el contenido de las Visiones de N'Zoth."
+--Corrupted Memento
+L["EoNLabel"] = "Ecos de Ny'alotha: "
+L["EoNTitle"] = "Ecos de Ny'alotha"
+L["EoNDescription"] = "Ahora que ha sido purificada de la corrupción de N'Zoth, \rMADRE podrá usar esta substancia para sintetizar esencias\r en la Cámara del Corazón."
+L["NoEoN"] = "Disponible en el contenido de las Visiones de N'Zoth."
+--Prismatic Manapearl
+L["PpearlLabel"] = "Perla maná: "
+L["PpearlTitle"] = "Perla de maná centelleante"
+L["PpearlDescription"] = "Una esfera brillante de energía Arcana encontrada en Nazjatar. \rSe puede comerciar con ella para obtener objetos poderosos y poco comunes \ro se puede usar para potenciar la armadura bentónica."
+L["NoPpearl"] = "Disponible en Nazjatar."
+--- Artifact Power
+L["noheart"] = "|cFFFF2e2eSin Corazón de Azeroth!"
+L["AzeriteLevel"] = "Nivel del Corazón de Azeroth:|cFFFFFFFF "
+L["AzeriteXP"] = "Poder Actual del Corazón de Azeroth:|cFFFFFFFF "
+L["AzeritetoLvL"] = "Poder para Subir:|cFFFFFFFF "
+L["AzeritetoMaximum"] = "Este Nivel es el Poder Maximo:|cFFFFFFFF "
+L["AzeriteLabel"] = "Azerita (P.A.): "
+L["AzeriteTitle"] = "Poder de Artefacto (Azerita)"
+L["AzeriteDescription"] = "El Poder de Artefacto eleva la Facultad del Corazón de Azeroth."
+
+------ Old pack
+--- Bloody Coin
+L["BloodyLabel"] = "Sangrienta: "
+L["BloodyTitle"] = "Moneda Sangrienta"
+L["BloodyDescription"] = "Obtenida de los cadáveres humeantes de enemigos derrotadosen\r en las brumas de la Isla Intemporal."
+L["NoBloody"] = "Disponible usando |cff0070ddIncensario de Agonía Eterna|r|cFFFF2e2e\ro|r |cff0070ddJuramento del Vigía del fuego|r |cFFFF2e2een Isla Intemporal.|r"
+--- Champion's Seal
+L["ChampionsLabel"] = "Sello: "
+L["ChampionsTitle"] = "Sello de Campeón"
+L["ChampionsDescription"] = "Otorgado por las hazañas en el Coliseo de los Cruzados."
+L["NoChampions"] = "Disponible en el Coliseo de los Cruzados, en Rasganorte."
+--- Epicurean's Award
+L["EpicureanLabel"] = "P. Epicúreo: "
+L["EpicureanTitle"] = "Premio Epicureo"
+L["EpicureanDescription"] = "Visita vendedores especial de cocina en Dalaran y las Capitales\rpara comparar recetas de cocina inusuales,\respecies e incluso un Gorro!"
+L["NoEpicurean"] = "Disponible a través de misiones de cocina\ren Antiguo Dalaran (Rasganorte) y las capitales."
+--- Good Fortune, Elder Charm of
+L["GFortuneLabel"] = "Buenas Fortuna: "
+L["GFortuneTitle"] = "Talisman Ancestral de Buena Fortuna"
+L["GFortuneDescription"] = "Poseer un talismán ancestral de buena fortuna permite\ral afortunado propietario apostar una moneda para obtener\runa recompensa extra en las Cámaras Mogu'shan,\rel Corazón del Miedo, la Veranda de la Primavera Eterna,\rdel Sha de la Ira o la banda guerrera de Salyis."
+--- Ironpaw Token
+L["IronpawLabel"] = "M. Zarpa Ferrea: "
+L["IronpawTitle"] = "Marca Zarpa Ferrea"
+L["IronpawDescription"] = "Representa tu crédito con la familia de cocineros Zarpa Férrea.\rSe puede canjear por ingredientes de cocina\ry otras cosas con el maestro de despensa en El Mercado del Alcor."
+L["NoIronpaw"] = "Disponible a través de misiones de cocina en Pandaria."
+--- Mark of the World Tree
+L["WorldTreeLabel"] = "Marcas: "
+L["WorldTreeTitle"] = "Marca del Arbol del Mundo"
+L["WorldTreeDescription"] = "Concedido por los guardianes de Hyjal. Se puede usar para\rcomprar refuerzos para la invasion de las Tierras de Fuego."
+L["NoWorldTree"] = "Disponible a través de misiones diarias en el Monte Hyjal."
+--- Mogu Rune of Fate
+L["MRuneLabel"] = "Runa Mogu: "
+L["MRuneTitle"] = "Runa del destino mogu"
+L["MRuneDescription"] = "Poseer una runa del destino mogu permite al afortunado propietario\robtener una recompensa extra del Solio del Trueno, de Nalak o de Oondasta."
+--- Timeless Coin
+L["TimelessLabel"] = "Intemporal: "
+L["TimelessTitle"] = "Moneda intemporal"
+L["TimelessDescription"] = "Sientes los fantasmales dedos de antiguos y futuros dueños de esta moneda jugando en tu mano,\rcomo si el propio tiempo se desdibujara.\rPuedes gastar estas monedas en la Isla Intemporal\rpara obtener poderosas y variadas recompensas."
+L["NoTimeless"] = "Disponible a través de Isla Intemporal."
+--- Warforged Seal
+L["WarforgedLabel"] = "Sello Guerra: "
+L["WarforgedTitle"] = "Sello forjado por la Guerra"
+L["WarforgedDescription"] = "Poseer un sello forjado por la guerra permite al afortunado propietario jugarse un sello para obtener una recompensa extra del Asedio de Orgrimmar, las pruebas de los Celestiales u Ordos."
 ------ Draenor pack
 --- Apexis Crystal
-L["ApexisLabel"] = "Cristales: "
-L["ApexisTitle"] = "Cristal Apexis"
-L["ApexisDescription"] = "The enduring legacy of a lost civilization,\rApexis crystals contain untold reserves\rof magical power." -- NO TRANSLATION
+L["ApexisLabel"] = "Cristal apexis: "
+L["ApexisTitle"] = "Cristal apexis"
+L["ApexisDescription"] = "Los cristales apexis, el eterno legado de una civilización perdida,\rcontienen una incalculable reserva de poder mágico."
+--- Artifact Fragment
+L["ArtifactLabel"] = "Fragmentos: "
+L["ArtifactTitle"] = "Fragmento de Artefacto"
+L["ArtifactDescription"] = "Un fragmento minúsculo de un artefacto perdido de Ashran,\rmuy preciado por los ogros de Ogrópolis."
+L["NoArtifact"] = "Mata enemigos o mobs en Ashran para coger algunos fragmentos."
 --- Dingy Iron Coins
-L["IronCoinsLabel"] = "Monedas: "
-L["IronCoinsTitle"] = "Monedas de Hiero deslucidas"
+L["IronCoinsLabel"] = "Monedas de Hierro deslucidas: "
+L["IronCoinsTitle"] = "Monedas de Hierro deslucidas"
 L["IronCoinsDescription"] = "Quizás son mas valiosas que lo que aparentan en la superficie."
 L["NoMaxCoin"] = "Disponible solo para Pícaro en Warlods of Draenor."
 --- Garrison Resources
 L["ResourcesLabel"] = "Recursos: "
 L["ResourcesTitle"] = "Recursos de la Ciudadela"
 L["ResourcesDescription"] = "Gana recursos para construir y expandir tu ciudadela."
-L["NoMaxRes"] = "Disponible a través de la guarnición en Warlords de Draenor."
+L["NoMaxRes"] = "Disponible a través de la Ciudadela en Warlords de Draenor."
 --- Oil
 L["OilLabel"] = "Petróleo: "
 L["OilTitle"] = "Petróleo"
-L["OilDescription"] = "Se encuentra principalmente en la Selva de Tanaan, el\rpetróleo se usa para reabastecer las misiones navales."
+L["OilDescription"] = "Se encuentra principalmente en la Selva de Tanaan,\rel petróleo se usa para reabastecer las misiones navales."
 L["NoMaxOil"] = "Disponible a través de Astillero en Warlords de Draenor."
 --- Primal Spirit
 L["SpiritLabel"] = "Primigenio: "
-L["SpiritTitle"] = "Espirito Primigenio"
+L["SpiritTitle"] = "Espiritu Primigenio"
 L["SpiritDescription"] = "Puede usarse para construir objetos o para comercializar\rcon un vendedor especial en tu ciudadela."
 --- Seal of Inevitable Fate
 L["InevitableLabel"] = "Destino Inevitable: "
-L["InevitableTitle"] = "Sello del Destino Inevitable"
-L["InevitableDescription"] = "Twists fate to provide an opportunity for\radditional treasure in Hellfire Citadel or\rMythic Dungeons." -- NO TRANSLATION
+L["InevitableTitle"] = "Sello de Destino Inevitable"
+L["InevitableDescription"] = "Altera el destino para ofrecer una oportunidad adicional\rde obtener botín en la Ciudadela del Fuego Infernal\ro en mazmorras míticas de Draenor."
 --- Seal of Tempered Fate
 L["TemperedLabel"] = "Destino Templado: "
-L["TemperedTitle"] = "Sello del destino templado"
-L["TemperedDescription"] = "Twists fate to provide an opportunity for\radditional treasure in Highmaul or\rBlackrock Foundry raids." -- NO TRANSLATION
+L["TemperedTitle"] = "Sello de destino templado"
+L["TemperedDescription"] = "Altera el destino para ofrecer una oportunidad adicional\rde obtener botín en Ogrópolis\ro en la Fundición Roca Negra."
+--- Valor
+L["ValorLabel"] = "Valor: "
+L["ValorTitle"] = "Valor"
+L["ValorDescription"] = "Una moneda empleada para aumentar el poder de potentes\robjetos creados y de objetos de la Ciudadela del Fuego Infernal,\rla Selva de Tanaan y las mazmorras de Draenor."
+L["NoValor"] = "Disponible a través de Mazmorras de Draenor."
 
 ------ Events pack
 --- Brewfest Prize Token
@@ -75,18 +185,40 @@ L["TrickyDescription"] = "Usada como moneda en el evento de Halloween.\rGeneralm
 ------ Legion pack
 --- Ancient Mana
 L["ManaLabel"] = "Antiguo M.: "
-L["ManaTitle"] = "Antiguo Mana"
-L["ManaDescription"] = "Magia concentrada de las antiguas\rlineas de ley bajo Suramar. "
+L["ManaTitle"] = "Manà antiguo"
+L["ManaDescription"] = "Magia concentrada de las antiguas lineas de ley bajo Suramar. "
 L["NoMana"] = "Adquirido en Suramar."
+--- Artifact Power
+L["noweapon"] = "|cFFFF2e2eNigún Artefacto!"
+L["ArticaftTitle"] = "Poder de Artefacto"
+L["ArticaftLevel"] = "Nivel de Arma Artefacto:|cFFFFFFFF "
+L["ArticaftSpend"] = "Puntos a gastar:|cFFFFFFFF "
+L["ArticaftXP"] = "Poder Actual del Arma:|cFFFFFFFF "
+L["ArticafttoLvL"] = "Poder para Subir:|cFFFFFFFF "
+L["ArticafttoMaximum"] = "Este Nivel es el Poder Maximo:|cFFFFFFFF "
+L["ArticaftKnowledge"] = "Nivel de Conocimiento:|cFFFFFFFF "
+L["ArticaftName"] = "Tu Artefacto: "
+L["noweaponttp"] = "Necesitas tener la expansión de Legión y\rhacer las misiones de clases para adquirirla."
+L["hint"] = "|cFFB4EEB4Consejo:|r |cFFFFFFFFClick-Izq abre la ventana del artefacto.|r\r\r"
+L["hidehint"] = "Oculta Consejo"
+L["hideknowledge"] = "Oculta el Conocimiento del Artefacto"
+L["researchOn"] = "Investigación en curso:|cFFFFFFFF "
+L["nextResearch"] = "Siguiente investigación en:|cFFFFFFFF "
+L["unknown"] = "Desconocido"
+--- Brawler's Gold
+L["BrawlerLabel"] = "O. Camorrista: "
+L["BrawlerTitle"] = "Oro del Camorrista"
+L["BrawlerDescription"] = "Otorgado a los vencedores de la Hermandad de camorristas.\rUsa esto para adquirir cartas de reyerta y otros objetos\rpara alterar el mundo en el vendedor de intercambio por oro de los camorristas."
+L["NoBrawler"] = "Disponible a través de las luchas del gremio de camorrista."
 --- Coins of Air
-L["CAirLabel"] = "Monedas del Aire: "
-L["CAirTitle"] = "Monedas del Aire"
-L["CAirDescription"] = "Evaporate into mist after a while."
-L["NoCAir"] = "Disponible solo para Rogue en Legión."
+L["CAirLabel"] = "Monedas de Aire: "
+L["CAirTitle"] = "Monedas de Aire"
+L["CAirDescription"] = "Se evaporan pasado un rato."
+L["NoCAir"] = "Disponible solo para Pìcaro en Legión."
 --- Curious Coin
 L["CuriousLabel"] = "M. Curiosa: "
 L["CuriousTitle"] = "Moneda Curiosa"
-L["CuriousDescription"] = "This unusual coin is warm to the touch\rand vibrates gently in your hand. Used\rto procure items from Xur'ios in Dalaran." -- NO TRANSLATION
+L["CuriousDescription"] = "Esta inusual moneda está caliente al tacto y vibra con suavidad en tu mano.\rSe usa para conseguir objetos de Xur'ios en Dalaran."
 L["NoCurious"] = "Encontrado en caídas aleatorias en Islas Abruptas."
 --- Drowned Mana
 L["DrownedLabel"] = "Mana ahogado: "
@@ -96,37 +228,41 @@ L["NoDrowned"] = "Pesca en el Retiro de Margoos, cerca de Dalaran."
 --- Legionfall War Supplies
 L["LegionfallLabel"] = "Suministros Legión: "
 L["LegionfallTitle"] = "Suministros del Ocaso de la Legión"
-L["LegionfallDescription"] = "Used to further the construction of valuable\rstructures on the Broken Shore." -- NO TRANSLATION
-L["NoLegionfall"] = "Can be acquired in Broken Shore." -- NO TRANSLATION
+L["LegionfallDescription"] = "Se utilizan para avanzar en la construcción\rde valiosas estructuras en la Costa Abrupta."
+L["NoLegionfall"] = "Encontrado en caídas aleatorias en la Costa Abrupta."
 --- Nethershard
 L["NetherLabel"] = "Fragmento del vacio: "
 L["NetherTitle"] = "Fragmento del vacio"
-L["NetherDescription"] = "Un fragmento de Nether cristalizado\rtraido a Azeroth por la Legión."
-L["NoNether"] = "Adquirida en Orilla Quebrada y en las\rinvasiones de la legión en Islas Quebradas."
+L["NetherDescription"] = "Un fragmento de vacío cristalizado\rtraido a Azeroth por la Legión."
+L["NoNether"] = "Adquirida en la Costa Abrupta y en las invasiones de la legión en Islas Abruptas."
 --- Order Resource
-L["OrderLabel"] = "Rec. de la Orden: "
-L["OrderTitle"] = "Recursos de la Orden"
-L["OrderDescription"] = "Usado para recrutrar tropas, hacer misiones, e\rinvestigar mejoras para tu casa de la Orden."
+L["OrderLabel"] = "Rec. de la Sede: "
+L["OrderTitle"] = "Recursos de la Sede"
+L["OrderDescription"] = "Se usan para reclutar tropas, realizar misiones\re investigar mejoras para la sede de tu clase."
 --- Seal of Broken Fate
 L["BrokenFateLabel"] = "Destino Alterado: "
 L["BrokenFateTitle"] = "Sello del Destino Alterado"
-L["BrokenFateDescription"] = "Twists fate to provide an opportunity for additional\rtreasure from Legion raid and Mythic dungeon bosses." -- NO TRANSLATION
+L["BrokenFateDescription"] = "Altera el destino para ofrecer una oportunidad adicional\rde obtener botín de jefes de bandas y mazmorras míticas de Legion."
 L["NoBrokenFate"] = "Puede adquirirse con el Archimago Lan'dalock, en Dalaran."
 --- Sightless Eye
 L["SightlessLabel"] = "Invidente: "
 L["SightlessTitle"] = "Ojo invidente"
-L["SightlessDescription"] = "Una moneda áspera, mano-acuñada. Lleva un símbolo similar\ral Kirin Tor, con una diferencia clave: el ojo está cerrado."
+L["SightlessDescription"] = "Una irregular moneda acuñada a mano.\rTiene un símbolo parecido al del Kirin Tor,\rcon una diferencia crucial: el ojo está cerrado."
 L["NoSightless"] = "Adquiridos en las Cloacas de Dalaran."
 --- Timeworn Artifact
-L["TimewornLabel"] = "Vetusto: "
+L["TimewornLabel"] = "Artefacto vetusto: "
 L["TimewornTitle"] = "Artefacto vetusto"
 L["TimewornDescription"] = "Un misterioso artefacto naga de las profundidades del Mare\rMagnum. El rey Mrgl-Mrgl los busca en Ensenada Marchita."
-L["NoTimeworn"] = "Disponible a través de misiones del mundo JcJ en Isla Quebrada."
---- Veiled Argunite (NO TRANSLATION)
-L["ArguniteLabel"] = "A. Velada: "
-L["ArguniteTitle"] = "Argunita Velada"
-L["ArguniteDescription"] = "This dull-colored crystal is highly valued by\rethereal traders, especially Thaumaturge\rVashreen aboard the Vindicaar."
+L["NoTimeworn"] = "Disponible a través de misiones del mundo JcJ en Azsuna."
+--- Veiled Argunite
+L["ArguniteLabel"] = "Argunita velada: "
+L["ArguniteTitle"] = "Argunita velada"
+L["ArguniteDescription"] = "Este cristal de color apagado es muy valioso para los comerciantes etéreos,\rsobre todo para el taumaturgo Vashreen,\ra bordo del Vindicaar."
 L["NoArgunite"] = "Adquirida en Argus."
+--- Wakening Essence
+L["WakeningLabel"] = "Esencia del despertar: "
+L["WakeningTitle"] = "Esencia del despertar"
+L["WakeningDescription"] = "El arcanomántico Vridiel la usa en Dalaran, sobre las Islas Abruptas,\rpara crear o mejorar objetos legendarios de Legion."
 
 ------ Old pack
 --- Bloody Coin
